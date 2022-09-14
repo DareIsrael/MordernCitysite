3f58f64a4119d89b6c13e6381715735b6844a1b4
Brightmac <macbookair@macbookairs-MacBook-Air.local>
2022-09-14 13:30:58 -0700

    finish

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
- ReactDOM.render(
-    (<BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>),
-    
-    document.getElementById('root'));
+
+ ReactDOM.render(<App />,document.getElementById('root'));
